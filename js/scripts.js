4cc69350a4cd5a1bf20f53927eb7da69b2c9afd9
taramonaghan <taramonaghan1022@gmail.com>
2021-05-06 17:10:00 -0400

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-alert('Hello world');
+let pokemonList = [{
+    name: "Bulbasaur", 
+    height: 0.7, 
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Ivysaur",
+    height: 1,
+    type: ['grass', 'poison']
+  },
+  {
+    name: "Vensaur",
+    height: 2,
+    type: ['grass', 'poison']
+  }]
 
-var favoriteFood = 'pizza'
-document.write(favoriteFood)
